4065f7342671d46b0b576e65fbfb91c40add4bc7
eason <acontentedmind@gmail.com>
2021-02-09 15:45:57 +0800

    可新增任務

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,5 +7,16 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new
+    if @task.save
+      redirect_to root_path, notice:'新增成功'
+    else
+      render :new
+    end
+  end
+
+  private
+  def params_task
+    params.require(:task).permit(:name, :content)
   end
 end
