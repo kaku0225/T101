c4a314c0c39707e251c676c62c2bb63e30dd7a64
eason <acontentedmind@gmail.com>
2021-02-12 12:19:28 +0800

    完成index,show,create的整合測試

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe "Tasks", type: :request do
     expect(response.body).to include('aa')
     expect(response.body).not_to include('zz')
   end
+
+  it "#show" do
+    get "/tasks/#{task_qq.id}"
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:show)
+    expect(response.body).to include('aa')
+  end
+
+  it "#create" do
+    params = { task: {name: 'name', content: 'content'}}
+    post "/tasks", params: params #後面那是自己取的，如果把上面那個的params改成bbb下面也可以改成 params: bbb
+    expect(response).to have_http_status(302)
+    expect(Task.last.name).to eq(params[:task][:name])
+  end
 end
