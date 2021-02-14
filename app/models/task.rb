981bb71870142dc9c42604ec78a7e9a8e2311966
eason <acontentedmind@gmail.com>
2021-02-14 13:08:13 +0800

    增加label的i18n

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :content, presence: true
+  # validates_presence_of :name, :message => :"任務名稱不可已是空白的"
 
   # validates :taiwanid, taiwanese_id: {message: "「您的身份證字號有誤，請確認後重新輸入」"}
 end
