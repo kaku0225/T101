4d99aac24d6fc933a5ec4212aeda65ad14fc12dc
eason <acontentedmind@gmail.com>
2021-02-14 11:44:20 +0800

    補上update destroy, update on false, createon false 整合測試

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -17,10 +17,53 @@ RSpec.describe "Tasks", type: :request do
     expect(response.body).to include('aa')
   end
 
+  it "#new" do
+    get "/tasks/new"
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:new)
+    expect(response.body).to include('name')
+    expect(response.body).not_to include('zz')
+  end
+
   it "#create" do
     params = { task: {name: 'name', content: 'content'}}
+    params1 = { task: {name: 'name1', content:''}}
+
     post "/tasks", params: params #後面那是自己取的，如果把上面那個的params改成bbb下面也可以改成 params: bbb
     expect(response).to have_http_status(302)
     expect(Task.last.name).to eq(params[:task][:name])
+
+    post "/tasks", params: params1
+    expect(response).not_to have_http_status(302)
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:new)
+  end
+
+  it "#edit" do
+    get "/tasks/#{task_qq.id}/edit"
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:edit)
+    expect(response.body).to include('name')
+    expect(response.body).not_to include('zz')
+  end
+
+  it "#update" do
+    params = { task: {name: 'name', content: 'content'}} 
+    params1 = { task: {name: 'name1', content:''}}
+
+    put "/tasks/#{task_qq.id}", params: params
+    expect(response).to have_http_status(302)
+    expect(task_qq.reload.name).to eq(params[:task][:name])
+
+    put "/tasks/#{task_qq.id}", params: params1
+    expect(response).not_to have_http_status(302)
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:edit)
+  end
+
+  it "#destroy" do
+    delete "/tasks/#{task_qq.id}"
+    expect { delete :destroy, params: {id: task_qq[:id]} }.to change{Task.all.size}.by(0)
+    expect(response).to have_http_status(302)
   end
 end
