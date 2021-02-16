63e8cd3c3025da3a835e2a21aeb900258dba81c2
eason <acontentedmind@gmail.com>
2021-02-16 19:19:16 +0800

    設定好aasm

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_100523) do
+ActiveRecord::Schema.define(version: 2021_02_16_103509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_02_16_100523) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "endtime"
+    t.string "state", default: "pending"
   end
 
 end
