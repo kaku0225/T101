454f4661a424b6c9a6671eed4ce5b7d8e584d6df
eason <acontentedmind@gmail.com>
2021-02-23 22:22:44 +0800

    增加edit 測試

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -45,4 +45,15 @@ RSpec.feature "Tasks", type: :feature do
     click_link "編輯"
     expect(find('.task-edit').text).to eq('Task Edit')
   end
+
+  scenario "update a  task" do
+    visit edit_task_path
+    fill_in "任務", with: "aaa"
+    fill_in "內容", with: "測試a"
+    
+    expect{click_button "新增任務"}.to change{Task.all.size}.by(1)
+    expect(page).to have_content "新增成功"
+    expect(page).to have_content "aaa"
+    expect(page).to have_content "測試a"
+  end
 end
