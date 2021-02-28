ed9d4917b3d34fc9a9792cec8320ecd1bdc1787b
eason <acontentedmind@gmail.com>
2021-02-28 22:15:31 +0800

    測試重整，剩下AASM未能測試

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  let!(:task_1) { Task.create(name: Faker::Lorem.sentence, content: Faker::Lorem.sentence)}
+  let!(:task_1) { Task.create(name: Faker::Lorem.sentence, content: Faker::Lorem.sentence, endtime:"2021-03-25 14:24:00.000000000 +0800", priority:"low")}
 
 
   scenario "click new" do
@@ -17,17 +17,20 @@ RSpec.feature "Tasks", type: :feature do
     visit new_task_path
     fill_in "任務", with: "aaa"
     fill_in "內容", with: "測試a"
+    fill_in "任務結束時間", with: 2021/03/25
     
     expect{click_button "新增任務"}.to change{Task.all.size}.by(1)
     expect(page).to have_content "新增成功"
     expect(page).to have_content "aaa"
     expect(page).to have_content "測試a"
   end
+  
 
   scenario "create a new task on false" do
     visit new_task_path
     fill_in "任務", with: "aaa"
     fill_in "內容", with: ""
+    fill_in "任務結束時間", with: 2021/03/25
     
     expect{click_button "新增任務"}.to change{Task.all.size}.by(0)
     expect(page).to have_content "任務內容不可空白"
@@ -59,5 +62,28 @@ RSpec.feature "Tasks", type: :feature do
   scenario "click 刪除" do
     visit root_path
     expect{click_link "刪除"}.to change{Task.all.size}.by(-1)
+  end
+
+  xscenario "click 狀態更新_progerss" do
+    visit root_path
+    click_link "更新狀態"
+    expect{task_1.state}.to eq 'progress'
+  end
+
+  xscenario "click 狀態更新_complete" do
+
+    transition_from(:offered).to(:wait_for_pap_confirmation).on_event(:pap_choosed).on(:status)
+
+    visit root_path
+    click_link "更新狀態"
+    expect{task_2.state}.to eq 'complete'
+  end
+
+  scenario "搜尋測試" do
+    visit root_path
+    fill_in "State 包含", with: "pending"
+    expect(page).to have_content "pending"
+    expect(page).not_to have_content "progress"
+    expect(page).not_to have_content "complete"
   end
 end
