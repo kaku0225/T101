835a746f58ede3cd57aaa84b2ad318cc4503fb54
eason <acontentedmind@gmail.com>
2021-03-05 13:53:00 +0800

    會員系統路徑完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_044932) do
+ActiveRecord::Schema.define(version: 2021_03_05_054450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2021_03_05_044932) do
     t.datetime "endtime"
     t.string "state", default: "pending"
     t.integer "priority"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "nickname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
