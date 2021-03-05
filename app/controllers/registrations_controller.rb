91812ada4e20fe2127992b841855396ee35ad72c
eason <acontentedmind@gmail.com>
2021-03-05 15:57:56 +0800

    可註冊

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,5 +4,16 @@ class RegistrationsController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: '註冊成功'
+    else
+      render :new
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :nickname)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   before_create :encrypt_password
 
   private
-  def encrypt_password
-    self.password = 
-  end
+    def encrypt_password
+      self.password = Digest::SHA1.hexdigest("a#{self.password}Z")
+    end
 end
